e0e4c3cd84d7a684346a59d68fb518826bf30051
Brad Mallow <bmallow@apple.com>
2024-06-04 14:06:27 -0700

    commit built files

--- docs/assets/js/do.js
+++ docs/assets/js/do.js
@@ -13188,7 +13188,7 @@ this["bmdotcom"]["templates"]["resumeView"] = function (obj) {
         __p += '\n      </ul>\n    </section>\n  </div>\n\n  <div class="primary-content-container">\n    <section class="summary">\n      <h2 class="resume-section-header">Summary</h2>\n\n      <p>' + ((__t = (currentPage.summary)) == null ? '' : __t) + '</p>\n    </section>\n\n    <section class="experiences screen-only">\n      <h2 class="resume-section-header">Professional Experience</h2>\n\n      ';
         _.each(currentPage.experience, function (job, index, list) {
             __p += '\n        ';
-            if (index <= 1) {
+            if (index <= 2) {
                 __p += '\n          <div class="experience">\n            <h3 itemscope itemprop="worksFor" itemtype="https://schema.org/Organization">' + ((__t = (job.title)) == null ? '' : __t) + ', <span itemprop="name">' + ((__t = (job.organization)) == null ? '' : __t) + '</span></h3>\n\n            <span class="job-location-period">\n              ';
                 print((job.location ? job.location + ' &#8212; ' : '') + job.period.start + ' to ' + (job.period.end ? job.period.end : 'present'));
                 __p += '\n            </span>\n\n            ';
